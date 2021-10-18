d2ef5f7c28e666bc86e76e0c9b42df332bb9b7a3
piet <pprotassow@posteo.de>
2021-10-18 15:16:34 +0200

    jest testing for the stats module

--- stats.js
+++ stats.js
@@ -1,16 +1,38 @@
-exports.sum = arr => arr.reduce((a, b) => a + b)
-exports.mean = arr => arr.reduce((a, b) => a + b) / arr.length
+const sum = (x, y) => x + y
+const square = x => x * x
 
-exports.stddev = arr => {
-    let mu = exports.mean(arr)
-    return Math.sqrt(
-        arr.reduce((a, b) => {
-            a = a + (b - mu) ** 2
-            return a
-        }, 0) /
-            (arr.length - 1)
+exports.mean = arr => arr.reduce(sum) / arr.length
+
+// exports.stddev = arr => {
+//     let mu = exports.mean(arr)
+//     return Math.sqrt(
+//         arr.reduce((a, b) => {
+//             a = a + (b - mu) ** 2
+//             return a
+//         }, 0) /
+//             (arr.length - 1)
+//     )
+// }
+exports.variance = arr => {
+    let m = exports.mean(arr)
+    return (
+        arr
+            .map(x => x - m)
+            .map(square)
+            .reduce(sum) /
+        (arr.length - 1)
     )
 }
 
+exports.stddev = arr => {
+    return Math.sqrt(exports.variance(arr))
+}
+
+//Covariance
+exports.cov = (arr_x, arr_y) => {
+    let X_m = exports.mean(arr_x)
+    let Y_m = exports.mean(arr_y)
+    return arr_x.map((x,id) => (x - X_m) * (arr_y[id] - Y_m)).reduce(sum) / (arr_x.length - 1)
+}
 
 // console.log(exports.stddev([1,2,1]))
---
+++ testing/spec.js
@@ -0,0 +1,36 @@
+const stats = require('../stats.js')
+
+describe('mean function', () => {
+  it('mean of array', () => {
+    expect(stats.mean([1,2,3])).toEqual(2);
+  });
+});
+describe('mean function', () => {
+  it('mean of array', () => {
+    expect(stats.mean([1,2])).toEqual(1.5);
+  });
+});
+
+describe('variance function', () => {
+  it('variance of array', () => {
+    expect(stats.variance([10, 12, 23, 23, 16, 23, 21, 16]).toFixed(4)).toEqual('27.4286');
+  });
+});
+
+describe('stdev function', () => {
+  it('standard deviation of array', () => {
+    expect(stats.stddev([1,2,1]).toFixed(4)).toEqual('0.5774');
+  });
+});
+describe('stdev function', () => {
+  it('standard deviation of array', () => {
+    expect(stats.stddev([10, 12, 23, 23, 16, 23, 21, 16]).toFixed(4)).toEqual('5.2372');
+  });
+});
+
+describe('cov function', () => {
+  it('covariance of two arrays', () => {
+    expect(stats.cov([10, 12, 23], [10, 12, 23])).toEqual(stats.variance([10, 12, 23]));
+  });
+});
+
