003b3288ee532bfe1108243e146110ce6bffa4a1
piet <pprotassow@posteo.de>
2021-10-19 11:46:23 +0200

    testing linear behavior of log(N)

--- testLog.py
+++ testLog.py
@@ -1,19 +1,21 @@
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+import numpy as np
 import math
 
-df = pd.read_json("data.json")
-X = list(df.x)
-Y = list(df.y)
-errors_Y = list(df.error_y)
+# X = [x for x in range(1, 10)]
+# Y = [20 * math.log2(y) for y in range(1, 10)]
+X = np.linspace(2, 8, 7)
+X = ([np.power(2, x) for x in X])
+Y = (2 * np.log2(X))
+# Y = [math.exp(y) for y in range(1, 10)]
 
 fig = go.Figure()
-# fig.add_trace(go.Scatter(x=X, y=Y, mode="markers", name="Experiment"))
-fig.add_trace(go.Scatter(x=X, y=Y, error_y=dict(type="data", array=errors_Y, visible=True), mode="markers", name="Experiment"))
+fig.add_trace(go.Scatter(x=X, y=Y, mode="markers", name="Experiment"))
 df = pd.read_json("fit.json")
-X = list(df.x)
-Y = list(df.y)
+# X = list(map(lambda x: math.log2(x), X))
+Y = np.sqrt(np.power(2, Y))
 fig.add_trace(
     go.Scatter(
         x=X,
@@ -31,6 +33,7 @@ fig.update_layout(
         color="RebeccaPurple"
     )
 )
+
 # fig.update_yaxes(type="log")
 # fig.update_xaxes(type="log")
 
